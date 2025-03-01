da634e96a0c7c1169e56fa505801f8986026dfde
mekanyanko <mekanyanko@gmail.com>
2025-03-02 04:58:43 +0900

    コンフリクトが発生した部分を修正
    コンフリクトが発生した部分を修正

--- lib/avatars_page.dart
+++ lib/avatars_page.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'dart:async';
+import 'dart:convert';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
